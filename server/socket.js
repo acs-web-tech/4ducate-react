043edf37813cbb2a5fc4b09d890530501caa06c2
acs-web-tech <aruncsedeptjacsi@gmail.com>
2024-11-07 20:39:56 +0530

    restore-commint

--- server/socket.js
+++ server/socket.js
@@ -1,11 +1,11 @@
 const { Server } = require("socket.io");
 
-const io = new Server({cors:{origin:"http://localhost:3001"}});
+const io = new Server({cors:{origin:"http://localhost:3000"}});
 
 io.on("connection", (socket) => {
 console.log("worked")
 });
-// io.on("message",(socket)=>{
-    
-// })
-io.listen(3000);
+io.on("message",(socket)=>{
+    console.log(socket)
+})
+io.listen(3001);
