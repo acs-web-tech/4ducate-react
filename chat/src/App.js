8d472047daece46b3621dda46eacb745d08ea723
acs-web-tech <aruncsedeptjacsi@gmail.com>
2024-12-01 20:05:32 +0530

    message-rendering-issue

--- chat/src/App.js
+++ chat/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { useState,useEffect,useCallback } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { Second } from "./Second"
 import useWebSocket, { ReadyState } from 'react-use-websocket';
-import {io} from "socket.io-client"
+import { io } from "socket.io-client"
 let sendarr = [
   {
+    id: 4,
     type: "sender",
     name: "Arun P",
     des: "Developer",
@@ -13,11 +14,13 @@ let sendarr = [
     time: "11:34am"
   },
   {
+    id: 5,
     type: "res",
     message: "Hello ",
     time: "11:34am"
   },
   {
+    id: 6,
     type: "sender",
     name: "Arun P",
     des: "Developer",
@@ -25,6 +28,7 @@ let sendarr = [
     time: "11:34am"
   },
   {
+    id: 7,
     type: "res",
     message: "Hello guys!",
     time: "11:34am"
@@ -35,120 +39,153 @@ let resarr = []
 function App() {
   // document.getElementById("usr_cir1").removeAttribute("aria-hidden");
   let [sendr_msg, setsendr_msg] = useState(sendarr)
+  let [isconnected,setCon] = useState(false)
   const t = new Date()
   const time = t.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
-  const socket = io("http://localhost:3001");  
-  useEffect(()=>{
-  socket.on("message",(data)=>{
+  if(!isconnected){
+  var socket = io("http://localhost:3001");
+  }
+  let count = 0
+  useEffect(() => {
+    socket.on("connection",(data)=>{
+      setCon(true)
+    })
+
+    socket.on("message", (data) => {
+       if(sendr_msg[sendr_msg.length-1].id != data.id){
+          sendarr.push(
+            {
+              type: "sender",
+              name: "Arun P",
+              des: "Developer",
+              message: data.message,
+              time: time
+            }
+          )
+          setsendr_msg(
+            [...sendarr ]
+          )
+        }
+             
+      
   
-    console.log(data)
-  setsendr_msg(
-    [...sendr_msg, {
-      type: "sender",
-      name: "Arun P",
-      des: "Developer",
-      message: data,
-      time: time
-    }]
-  )
- })
-},[])
- 
+      })
+    
 
-  const [isSenderTurn, setIsSenderTurn] = useState(true);
-  const bdyarea = document.querySelector('.bdy')
-  // const msg_tip = document.querySelector('.msg_tip')
+}, [isconnected])
 
 
-  const Store_value = (event) => {
-    const inputarea = document.querySelector('.typing')
-    if (inputarea.value.trim() !== "") {
-      if (isSenderTurn) {
-        setsendr_msg([...sendr_msg, {
-          type: "sender",
-          name: "Arun P",
-          des: "Developer",
-          message: inputarea.value,
-          time: time
-        }])
+const [isSenderTurn, setIsSenderTurn] = useState(true);
+const bdyarea = document.querySelector('.bdy')
+// const msg_tip = document.querySelector('.msg_tip')
 
-      }
-      else {
-        setsendr_msg([...sendr_msg, {
-          type: "res",
-          message: inputarea.value,
-          time: time
-        }])
+
+const Store_value = (event) => {
+  const inputarea = document.querySelector('.typing')
+  if (inputarea.value.trim() !== "") {
+    if (isSenderTurn) {
+      setsendr_msg([...sendr_msg, {
+        type: "sender",
+        name: "Arun P",
+        des: "Developer",
+        message: inputarea.value,
+        time: time
+      }])
+
+    }
+    else {
+      setsendr_msg([...sendr_msg, {
+        type: "res",
+        message: inputarea.value,
+        time: time
+      }])
 
 
 
 
-      }
-
-      inputarea.value = ''
-      window.onload = function () {
-        document.getElementById('usr_cir1').removeAttribute('aria-hidden');
-        document.getElementById('usr_cir2').removeAttribute('aria-hidden');
-      };
     }
 
+    inputarea.value = ''
+    window.onload = function () {
+      document.getElementById('usr_cir1').removeAttribute('aria-hidden');
+      document.getElementById('usr_cir2').removeAttribute('aria-hidden');
+    };
   }
-    return (
-      <>
-        <div class='chat_container'>
-          <header class='head'>
-            <div className='icon_contr'>
-              <i id='usr_cir1 ' className='icon' class="fa-solid fa-circle-user"></i>
-              <i id='usr_cir2 ' className='icon' class="fa-solid fa-circle-user"></i>
-            </div>
-            <div>
-              <h4 className='title'>Team Unicorns</h4>
-              <h6>last seen 45 minutes ago</h6>
-            </div>
-            <i id='menu_icon' class="fa-solid fa-ellipsis"></i>
-          </header>
-          <div class='bdy'>
-            {/* <div class='msg_tip' ></div> */}
-            <Message sender={sendr_msg} isSenderTurn={isSenderTurn}></Message>
-          </div>
-          <footer class='footer'>
-            <i id='smil_emj' class="fa-solid fa-face-grin"></i>
-            <input class='typing' type='text' placeholder='Start typing...'></input>
-            <i class="fa-solid fa-at"></i><i onClick={Store_value} class="fa-regular fa-paper-plane"></i>
-          </footer>
+
+}
+return (
+  <>
+    <div class='chat_container'>
+      <header class='head'>
+        <div className='icon_contr'>
+          <i id='usr_cir1 ' className='icon' class="fa-solid fa-circle-user"></i>
+          <i id='usr_cir2 ' className='icon' class="fa-solid fa-circle-user"></i>
         </div>
+        <div>
+          <h4 className='title'>Team Unicorns</h4>
+          <h6>last seen 45 minutes ago</h6>
+        </div>
+        <i id='menu_icon' class="fa-solid fa-ellipsis"></i>
+      </header>
+      <div class='bdy'>
+        {/* <div class='msg_tip' ></div> */}
+        <Message sender={sendr_msg} isSenderTurn={isSenderTurn}></Message>
+      </div>
+      <footer class='footer'>
+        <i id='smil_emj' class="fa-solid fa-face-grin"></i>
+        <input class='typing' type='text' placeholder='Start typing...'></input>
+        <i class="fa-solid fa-at"></i><i onClick={Store_value} class="fa-regular fa-paper-plane"></i>
+      </footer>
+    </div>
 
 
-      </>
-    );
+  </>
+);
 
 }
 
 
-  function Message(props) {
-    // console.log(props.res.length)
-    return (
-      <div>
+function Message(props) {
+  // console.log(props.res.length)
+  return (
+    <div>
 
 
-        {props.sender.length > 0 ?
-          props.sender.map((value, index) => {
+      {props.sender.length > 0 ?
+        props.sender.map((value, index) => {
 
-            return (
-              <>
-                {value.type == "sender" ? <div className='sender'>
-                  <div className='message mes-sender'>
-                    <div className='use-case'>
-                      <div className='user-name'>
-                        {value.name}
-                      </div>
-                      <div className='des'>{value.des}</div>
+          return (
+            <>
+              {value.type == "sender" ? <div className='sender'>
+                <div className='message mes-sender'>
+                  <div className='use-case'>
+                    <div className='user-name'>
+                      {value.name}
                     </div>
+                    <div className='des'>{value.des}</div>
+                  </div>
+                  {value.message}
+                  <div className='tip right'>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="15" height="12" viewBox="0 0 15 12" fill="none">
+                      <path d="M9 0C9 0 3.26206 0 1.8 0C0.33795 0 3.14713e-05 1.5 1.35003 3C2.70003 4.5 8.50063 9.5 9 11C9.49936 12.5 9 0 9 0Z" fill="#F2F2F7" />
+                      <rect width="6" height="12" transform="matrix(-1 0 0 1 15 0)" fill="#F2F2F7" />
+                    </svg>
+                  </div>
+                  <div className='time'>
+                    {value.time}
+                  </div>
+                </div>
+
+              </div> : ""}
+              {value.type == "res" ? <div className='res-cont'>
+
+                <div className='recive'>
+                  <div className='message'>
                     {value.message}
-                    <div className='tip right'>
+                    <div className='tip'>
                       <svg xmlns="http://www.w3.org/2000/svg" width="15" height="12" viewBox="0 0 15 12" fill="none">
-                        <path d="M9 0C9 0 3.26206 0 1.8 0C0.33795 0 3.14713e-05 1.5 1.35003 3C2.70003 4.5 8.50063 9.5 9 11C9.49936 12.5 9 0 9 0Z" fill="#F2F2F7" />
-                        <rect width="6" height="12" transform="matrix(-1 0 0 1 15 0)" fill="#F2F2F7" />
+                        <path d="M6 0C6 0 11.7379 0 13.2 0C14.6621 0 15 1.5 13.65 3C12.3 4.5 6.49937 9.5 6 11C5.50064 12.5 6 0 6 0Z" fill="#007AFF" />
+                        <rect width="6" height="12" fill="#007AFF" />
                       </svg>
                     </div>
                     <div className='time'>
@@ -156,40 +193,23 @@ function App() {
                     </div>
                   </div>
 
-                </div> : ""}
-                {value.type == "res" ? <div className='res-cont'>
-
-                  <div className='recive'>
-                    <div className='message'>
-                      {value.message}
-                      <div className='tip'>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="15" height="12" viewBox="0 0 15 12" fill="none">
-                          <path d="M6 0C6 0 11.7379 0 13.2 0C14.6621 0 15 1.5 13.65 3C12.3 4.5 6.49937 9.5 6 11C5.50064 12.5 6 0 6 0Z" fill="#007AFF" />
-                          <rect width="6" height="12" fill="#007AFF" />
-                        </svg>
-                      </div>
-                      <div className='time'>
-                        {value.time}
-                      </div>
-                    </div>
-
-                  </div>
-                </div> : ""}
-              </>
-            )
+                </div>
+              </div> : ""}
+            </>
+          )
 
 
 
 
-          })
-          : ""}
+        })
+        : ""}
 
-      </div >
+    </div >
 
-    )
+  )
 
-  }
+}
 
 
 
-  export default App;
+export default App;
--- server/socket.js
+++ server/socket.js
@@ -5,8 +5,9 @@ const io = new Server(httpServer,{cors:{origin:"http://localhost:3000"}});
 
 io.on("connection", (socket) => {
 
-  socket.emit("message","hello ")
-  socket.emit("message","hello 2")
+  socket.emit("message",{message:"hello",id:1})
+  socket.emit("message",{message:"hello 2",id:2})
+  socket.emit("message",{message:"hello 3",id:3})
 
 });
 io.on("message",(socket)=>{
